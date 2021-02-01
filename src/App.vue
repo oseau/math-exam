2e13674912545366b65d9733809faae92fe6f358
yang <oseau@me.com>
2021-02-02 00:40:53 +0800

    add example

--- src/App.vue
+++ src/App.vue
@@ -1,19 +1,36 @@
 <template>
-  <img alt="Vue logo" src="./assets/logo.png" />
-  <HelloWorld msg="Hello Vue 3 + Vite" />
+  <div class="flex mb-20" v-for="row in rows" :key="row">
+    <div
+      class="flex-auto"
+      v-for="expr in row"
+      :key="expr"
+      v-html="String.raw`\[${expr}\]`"
+    ></div>
+  </div>
 </template>
 
 <script setup>
-import HelloWorld from './components/HelloWorld.vue'
+const rows = String.raw`
+2580-2547
+1596-1296
+365+97
+365-97
+150-85-15
+-----------------
+1450-375-203-625
+-----------------
+x = {-b \pm \sqrt{b^2-4ac} \over 2a}`
+  .split(/\r?\n/)
+  .filter((line) => line.length > 0)
+  .reduce(
+    (acc, cur) => {
+      if (cur === "-----------------") {
+        acc.push([]);
+      } else {
+        acc[acc.length - 1].push(cur);
+      }
+      return acc;
+    },
+    [[]]
+  );
 </script>
-
-<style>
-#app {
-  font-family: Avenir, Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-  margin-top: 60px;
-}
-</style>
